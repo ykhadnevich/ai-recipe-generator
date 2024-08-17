1abdc40897f70490b2a2470396f00bdb989dfe6e
Yan_Khadnevich <90506265+ykhadnevich@users.noreply.github.com>
2024-08-17 13:54:08 +0300

    connect to bedrock

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,29 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
+import { defineBackend } from "@aws-amplify/backend";
+import { data } from "./data/resource";
+import { PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { auth } from "./auth/resource";
 
-/**
- * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
- */
-defineBackend({
-  auth,
-  data,
+const backend = defineBackend({
+    auth,
+    data,
 });
+
+const bedrockDataSource = backend.data.resources.graphqlApi.addHttpDataSource(
+    "bedrockDS",
+    "https://bedrock-runtime.us-east-1.amazonaws.com", // Corrected URL
+    {
+        authorizationConfig: {
+            signingRegion: "us-east-1",
+            signingServiceName: "bedrock",
+        },
+    }
+);
+
+bedrockDataSource.grantPrincipal.addToPrincipalPolicy(
+    new PolicyStatement({
+        resources: [
+            "arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude-3-sonnet-20240229-v1:0", // Corrected ARN
+        ],
+        actions: ["bedrock:InvokeModel"],
+    })
+);
---
+++ src/App.tsx
@@ -0,0 +1,92 @@
+import { FormEvent, useState } from "react";
+import { Loader, Placeholder } from "@aws-amplify/ui-react";
+import "./App.css";
+import { Amplify } from "aws-amplify";
+import { Schema } from "../amplify/data/resource";
+import { generateClient } from "aws-amplify/data";
+import outputs from "../amplify_outputs.json";
+
+
+import "@aws-amplify/ui-react/styles.css";
+
+Amplify.configure(outputs);
+
+const amplifyClient = generateClient<Schema>({
+    authMode: "userPool",
+});
+
+function App() {
+    const [result, setResult] = useState<string>("");
+    const [loading, setLoading] = useState(false);
+
+    const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        setLoading(true);
+
+        try {
+            const formData = new FormData(event.currentTarget);
+
+            const { data, errors } = await amplifyClient.queries.askBedrock({
+                ingredients: [formData.get("ingredients")?.toString() || ""],
+            });
+
+            if (!errors) {
+                setResult(data?.body || "No data returned");
+            } else {
+                console.log(errors);
+            }
+
+
+        } catch (e) {
+            alert(`An error occurred: ${e}`);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <div className="app-container">
+            <div className="header-container">
+                <h1 className="main-header">
+                    Meet Your Personal
+                    <br />
+                    <span className="highlight">Recipe AI</span>
+                </h1>
+                <p className="description">
+                    Simply type a few ingredients using the format ingredient1,
+                    ingredient2, etc., and Recipe AI will generate an all-new recipe on
+                    demand...
+                </p>
+            </div>
+            <form onSubmit={onSubmit} className="form-container">
+                <div className="search-container">
+                    <input
+                        type="text"
+                        className="wide-input"
+                        id="ingredients"
+                        name="ingredients"
+                        placeholder="Ingredient1, Ingredient2, Ingredient3,...etc"
+                    />
+                    <button type="submit" className="search-button">
+                        Generate
+                    </button>
+                </div>
+            </form>
+            <div className="result-container">
+                {loading ? (
+                    <div className="loader-container">
+                        <p>Loading...</p>
+                        <Loader size="large" />
+                        <Placeholder size="large" />
+                        <Placeholder size="large" />
+                        <Placeholder size="large" />
+                    </div>
+                ) : (
+                    result && <p className="result">{result}</p>
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default App;
